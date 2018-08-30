fe017dc35845d910e7e1f4f2e342352170ae3b4e
Stephen Chu <13208971+stephenchu530@users.noreply.github.com>
2018-08-30 13:29:55 -0700

    Revert "fix: suppress error code and message when in non-ia mode"

--- builtins2.c
+++ builtins2.c
@@ -52,11 +52,8 @@ void change_dir(sev_t *sev)
 	}
 	else if (targetdir[0] == '-')
 	{
-		if (sev->ia_mode)
-		{
-			sev->errmsg = illegaloptions(sev);
-			sev->error = 1;
-		}
+		sev->errmsg = illegaloptions(sev);
+		sev->error = 1;
 		return;
 	}
 
@@ -65,11 +62,8 @@ void change_dir(sev_t *sev)
 	/* throw error message when chdir fails */
 	if (ret_val == -1)
 	{
-		if (sev->ia_mode)
-		{
-			sev->errmsg = invaliddir(sev);
-			sev->error = 1;
-		}
+		sev->error = 1;
+		sev->errmsg = invaliddir(sev);
 	}
 
 	/* change pwd in env list and reset oldpwd */
