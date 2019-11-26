b46ba0b6f91512ad11d42dd2ef011b5c3c4fc8d1
izizatoichi <58192089+izizatoichi@users.noreply.github.com>
2019-11-27 00:31:38 +0100

    Update shellhelper.h

--- shellhelperfiles/shellhelper.h
+++ shellhelperfiles/shellhelper.h
@@ -8,20 +8,20 @@ char **make_array_str(char *s, const char *delim, sev_t *sev);
 int actions(sev_t *sev);
 
 /* shellhelper02.c */
-char *_getenv(char *envar, sev_t *sev);
+char *_getenvironment(char *envar, sev_t *sev);
 char *pathfind(sev_t *sev);
 void cl_sev(sev_t *sev);
 char **make_evp_array(sev_t *sev);
 int _setenv_help(sev_t *sev, char *tofind, char *cwd);
 
 /* shellhelper03.c */
-sev_t *initialize_shell_env(sev_t *sev, char **av, char **ev);
-list_t *read_env(sev_t *sev, char **ev);
-void display_error(sev_t *sev);
-void var_expansion(sev_t *sev);
-int print_alias_val(sev_t *sev, char *key, char *value, int flag);
+sev_t *init_shell_env(sev_t *sev, char **av, char **ev);
+list_t *read_environment(sev_t *sev, char **ev);
+void dis_error(sev_t *sev);
+void var_expan(sev_t *sev);
+int p_alias_val(sev_t *sev, char *key, char *value, int flag);
 
 /* shellhelper04.c */
-void process_input(char *cmd, sev_t *sev);
-void display_banner(int ia_mode);
-void check_alias(sev_t *sev);
+void process_inputs(char *cmd, sev_t *sev);
+void dis_banner(int ia_mode);
+void checker_alias(sev_t *sev);
