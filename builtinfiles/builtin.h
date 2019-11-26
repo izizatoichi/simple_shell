78947ac508de75650ad28833ae9922d931fd8e6d
izizatoichi <58192089+izizatoichi@users.noreply.github.com>
2019-11-26 22:46:32 +0100

    Update builtin.h

--- builtinfiles/builtin.h
+++ builtinfiles/builtin.h
@@ -22,20 +22,6 @@ typedef struct builtin
 	void (*func)(sev_t *);
 } built_t;
 
-/**
- * struct help_list - structure of argument and help file
- * @arg: arg string
- * @file: path of file
- *
- * Description: Struct contain str arg and path to the file where its
- * help desc is found
- */
-typedef struct help_list
-{
-	char *arg;
-	char *file;
-} help_t;
-
 /* builtin01.c */
 void exit_sh(sev_t *sev);
 void _printenv(sev_t *sev);
@@ -47,6 +33,5 @@ int check_builtin(sev_t *sev);
 void change_dir(sev_t *sev);
 void history(sev_t *sev);
 void alias(sev_t *sev);
-void _help(sev_t *sev);
 
 #endif
