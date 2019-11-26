2aeda0d58711034b35ff0c4e3374ffe0eac961e7
izizatoichi <58192089+izizatoichi@users.noreply.github.com>
2019-11-26 22:56:24 +0100

    Update and rename errors.h to error.h

--- errorfiles/error.h
+++ errorfiles/error.h
@@ -1,18 +1,18 @@
-#ifndef _ERRORS_H_
-#define _ERRORS_H_
+#ifndef _ERROR_H_
+#define _ERROR_H_
 
 #include "../shell.h"
 
-/* errors1.c */
+/* error01.c */
 char *filenotfound(sev_t *sev);
 char *illegalnum(sev_t *sev);
 char *permdenied(sev_t *sev);
 char *invaliddir(sev_t *sev);
 char *invalidalias(sev_t *sev, int reset);
 
-/* errors2.c */
+/* error02.c */
 char *helpfilenotfound(sev_t *sev);
 char *illegaloptions(sev_t *sev);
 char *invalidenv(sev_t *sev);
 
-#endif /* _ERRORS_H_ */
+#endif /* _ERROR_H_ */
