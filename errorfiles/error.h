52e8f887ab674e6ce8d4a29f9efa77d565a8c48d
izizatoichi <58192089+izizatoichi@users.noreply.github.com>
2019-11-26 23:04:28 +0100

    Update error.h

--- errorfiles/error.h
+++ errorfiles/error.h
@@ -4,15 +4,15 @@
 #include "../shell.h"
 
 /* error01.c */
-char *filenotfound(sev_t *sev);
-char *illegalnum(sev_t *sev);
-char *permdenied(sev_t *sev);
-char *invaliddir(sev_t *sev);
-char *invalidalias(sev_t *sev, int reset);
+char *fnotfound(sev_t *sev);
+char *illnum(sev_t *sev);
+char *perdenied(sev_t *sev);
+char *invaldir(sev_t *sev);
+char *invalalias(sev_t *sev, int reset);
 
 /* error02.c */
-char *helpfilenotfound(sev_t *sev);
-char *illegaloptions(sev_t *sev);
-char *invalidenv(sev_t *sev);
+char *helpfnotfound(sev_t *sev);
+char *illoptions(sev_t *sev);
+char *invaldenv(sev_t *sev);
 
 #endif /* _ERROR_H_ */
