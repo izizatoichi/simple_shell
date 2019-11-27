a7b7a70264485ba81b0e93734a46d83e566094c1
izizatoichi <58192089+izizatoichi@users.noreply.github.com>
2019-11-27 19:10:36 +0100

    Update and rename linklists.h to linklist.h

--- linklist.h
+++ linklist.h
@@ -1,15 +1,15 @@
-#ifndef _LINKLISTS_H_
-#define _LINKLISTS_H_
+#ifndef _LINKLIST_H_
+#define _LINKLIST_H_
 
 #include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
- * @key: void * type pointer to the key
- * @value: void * type poiner to the value
+ * struct list_s - sing linked list
+ * @key: void * type p to the key
+ * @value: void * type p to the value
  * @next: struct list_s ptr to the next node
  *
- * Description: singly linked list used in the shell project
+ * Description: sing linked list used in shell project
  */
 typedef struct list_s
 {
@@ -18,10 +18,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* linklists1.c */
+/* linklist01.c */
 list_t *add_node(list_t **head, void *key, void *value);
 list_t *reverse_list(list_t **head);
 void free_list(list_t **head, int flag);
 int delete_node_at_index(list_t **head, unsigned int index);
 
-#endif /* _LINKLISTS_H_ */
+#endif /* _LINKLIST_H_ */
