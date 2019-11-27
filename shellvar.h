9daf74c613c6babef198186379d9eb2f6032b8df
izizatoichi <58192089+izizatoichi@users.noreply.github.com>
2019-11-27 20:26:47 +0100

    Update shellvar.h

--- shellvar.h
+++ shellvar.h
@@ -4,26 +4,25 @@
 #include "shell.h"
 
 /**
- * struct sev_s - shell environment variables
+ * struct sev_s - shell env vars
  * @skywalker: flag to continue or not continue with shell
- * @ia_mode: interactive mode - 1 yes, 0 no
  * @log: ptr to log link list for the current shell session
- * @log_cnt: number of log entries for the curent shell session
+ * @log_cnt: num of log entries for the curent shell session
  * @cmd_cnt: cmd counter separate from log counter
- * @mem: ptr to memory allocations for the current shell session
- * @env: ptr to the os environment variables
- * @evp: ptr to environment variables in array
+ * @mem: ptr to mem alloc for the current shell session
+ * @env: ptr to the os env vars
+ * @evp: ptr to env vars in arr
  * @input: ptr to the user's input
  * @p_input: ptr to the procssed input
- * @error: error number from current command
- * @errmsg: error message associated with the error number
+ * @error: error num from current command
+ * @errmsg: error mess associated with the error num
  * @olderror: error from previous command
  * @pid: pid of current shell
  * @alias: ptr to link list of all alias created by user
  * @cmd_q: ptr to the list of commands in the queue
- * @shell_d: current working directory
+ * @shell_d: current working dir
  * @arg0: command executed to run shell
- * Description: Struct contain all the shell env vars.
+ * Description: Struct contain all shell env vars.
  * This one struct will be passed from func to func and contain
  * all the shell env vars for the current session.
  */
