d057601faedf3021261d549520b03927d5583bb9
izizatoichi <58192089+izizatoichi@users.noreply.github.com>
2019-11-27 19:22:17 +0100

    Update and rename log1.c to log01.c

--- log01.c
+++ log01.c
@@ -1,9 +1,9 @@
 #include "log.h"
 
 /**
- * add_log - add input from user to log
- * @sev: ptr to shell environment variable link list
- * Return: 1 for success, 0 for fail
+ * add_log - add input from the user to log
+ * @sev: ptr to shell env var link list
+ * Return: 1 in success, 0 in fail
  */
 int add_log(sev_t *sev)
 {
@@ -17,20 +17,20 @@ int add_log(sev_t *sev)
 }
 
 /**
- * write_log - writes the current sessions' log to a file
- * @sev: ptr to the shell environment variable
- * Return: 1 if success, 0 for fail
+ * write_log - write the current sessions' log to a file
+ * @sev: ptr to the shell env var
+ * Return: 1 in success, 0 in fail
  */
 int write_log(sev_t *sev)
 {
 	char *fullpath = NULL, *entry = NULL;
 	list_t *walker = NULL;
-	int fd = 0, len = 0;
+	int fds = 0, len = 0;
 
 	fullpath = _strcat(_getenv("HOME", sev), "/", &sev->mem);
 	fullpath = _strcat(fullpath, LOGFILE, &sev->mem);
-	fd = open(fullpath, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
+	fds = open(fullpath, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fds == -1)
 	{
 		return (0);
 	}
@@ -41,43 +41,43 @@ int write_log(sev_t *sev)
 		entry = walker->value;
 		entry = _strcat(entry, "\n", &sev->mem);
 		len = _strlen(entry);
-		if (write(fd, entry, len) < len)
+		if (write(fds, entry, len) < len)
 		{
 			close(fd);
 			return (0);
 		}
 		walker = walker->next;
 	}
-	close(fd);
+	close(fds);
 	return (1);
 }
 
 /**
- * get_log_count - reads and gets the log file count
- * @sev: ptr to the shell environment variable struct
- * Return: number of entries in log file, 0 if empty or fail
+ * get_log_count - read and get log file count
+ * @sev: ptr to shell env var struct
+ * Return: num of entries in log file, 0 in empty or fail
  */
 int get_log_count(sev_t *sev)
 {
 	char *fullpath = NULL, buffer[MAXBUFREAD];
-	int fd = 0;
+	int fds = 0;
 	size_t size = MAXBUFREAD, lines = 0;
 	ssize_t numread = 1, index = 0;
 
 	fullpath = _strcat(_getenv("HOME", sev), "/", &sev->mem);
 	fullpath = _strcat(fullpath, LOGFILE, &sev->mem);
 
-	fd = open(fullpath, O_RDONLY);
-	if (fd == -1)
+	fds = open(fullpath, O_RDONLY);
+	if (fds == -1)
 	{
 		return (0);
 	}
 	while (numread)
 	{
-		numread = read(fd, &buffer, size);
+		numread = read(fds, &buffer, size);
 		if (numread == -1)
 		{
-			close(fd);
+			close(fds);
 			return (0);
 		}
 		for (index = 0; index < numread; index++)
