005b59e4cd7730d5d817b0394e61f1beeacc7fe7
izizatoichi <58192089+izizatoichi@users.noreply.github.com>
2019-11-27 11:39:15 +0100

    Update _getline.c

--- _getline.c
+++ _getline.c
@@ -1,82 +1,82 @@
 #include "shell.h"
 
 /**
- * _getline - gets line from file descriptor
- * @lineptr: pointer to buffer
+ * _getline - get line from file descrip
+ * @lineptr: p to buff
  * @n: size of buffer
- * @fd: file descriptor
- * @mt: pointer to memory tracker link list
+ * @fd: file descrip
+ * @mt: p to memo tracker link list
  *
- * Description: Function will read line from file descriptor and put line into
- * buffer. If buffer passed into function is null, a newly allocated buffer
- * will be created for user. Buffer will resize if characters read exceeds
- * buffer size. The "lineptr" is then set as buffer.
+ * Description: Func will read line from file descriptor and put line into
+ * buff. If buff passed into func is null, a new alloc buff
+ * will be created for user. Buff will resize if chars read exceeds
+ * buff size. The "lineptr" is then set as buff.
  *
- * Return: number of characters read or -1 upon failure
+ * Return: num of chars read or -1 upon fail
  */
 ssize_t _getline(char **lineptr, size_t *n, int fd, list_t **mt)
 {
-	static char *buf;
+	static char *buff;
 	ssize_t char_read = 0, char_to_read = BUF_SIZE / 2;
 	ssize_t buf_size = BUF_SIZE, count = 0;
 
-	buf = malloc(buf_size);
+	buff = malloc(buf_size);
 	add_node(mt, NULL, buf);
 
-	if (!buf)
+	if (!buff)
 		return (-1);
 
 	if (!lineptr || !n)
 		return (-1);
 	if (*lineptr)
 	{
-		buf = *lineptr;
+		buff = *lineptr;
 		buf_size = *n;
 		if (*n == 1)
 			char_to_read = 1;
 		else
 			char_to_read = buf_size / 2;
 	}
-	reset_buffer(buf, buf_size);
-	char_read = read(fd, buf + count, char_to_read);
+	reset_buffer(buff, buf_size);
+	char_read = read(fd, buff + count, char_to_read);
 	if (char_read == -1)
 	{
-		free(buf);
+		free(buff);
 		return (-1);
 	}
 	if (char_read == 0)
 		return (-2);
 
-	char_read = _strlen(buf);
+	char_read = _strlen(buff);
 	count += char_read;
 	if (count > 0)
 	{
-		if (buf[count - 1] == '\n')
+		if (buff[count - 1] == '\n')
 			char_read = 0;
 	}
 	while (char_read)
 	{
 		if (count >= buf_size - char_to_read)
 		{
-			buf = (char *)_realloc(buf, buf_size, buf_size * 2, mt);
+			buff = (char *)_realloc(buff, buf_size, buf_size * 2, mt);
 			buf_size *= 2;
 		}
-		char_read = read(fd, buf + count, char_to_read);
+		char_read = read(fd, buff + count, char_to_read);
 		if (char_read == -1)
 			return (-1);
 		char_read = _strlen(buf) - count;
 		count += char_read;
-		if (buf[count - 1] == '\n')
+		if (buff[count - 1] == '\n')
 			break;
 	}
-	if (buf[count - 1] != '\n')
+	if (buff[count - 1] != '\n')
 	{
-		*lineptr = buf;
+		*lineptr = buff;
 		*n = buf_size;
 		write(STDIN_FILENO, "\n", 1);
 		return (-3);
 	}
-	*lineptr = buf;
+	*lineptr = buff;
 	*n = buf_size;
 	return (count);
 }
